0436ca8fbaff89021945a864e1989e797df32515
sahanpramudith <pramuditsahan466@gmail.com>
2024-08-07 07:52:16 +0530

    add main mathord

--- src/main/java/edu/icet/crm/Demo.java
+++ src/main/java/edu/icet/crm/Demo.java
@@ -1,4 +1,12 @@
 package edu.icet.crm;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Demo {
+    public static void main(String args[]){
+        SpringApplication.run(Demo.class);
+    }
+
 }
