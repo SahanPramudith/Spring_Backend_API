50047f6cd709a77c49616031aeca5df19741fc30
sahanpramudith <pramuditsahan466@gmail.com>
2024-08-07 08:22:32 +0530

    add  mathord

---
+++ src/main/java/edu/icet/crm/service/StudentService.java
@@ -0,0 +1,14 @@
+package edu.icet.crm.service;
+
+import edu.icet.crm.model.Student;
+
+public class StudentService {
+   public Student getstudent(){
+       Student student=new Student();
+       student.setEmail("sahan@");
+       student.setName("sahan");
+       student.setContacNumber("0741600386");
+
+       return student;
+   }
+}
