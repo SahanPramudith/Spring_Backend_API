30e2ca8d0d0f04dbd20f8cd6e06f9c57dba2c569
sahanpramudith <pramuditsahan466@gmail.com>
2024-08-07 08:20:02 +0530

    add  mathord

---
+++ src/main/java/edu/icet/crm/model/Student.java
@@ -0,0 +1,15 @@
+package edu.icet.crm.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class Student {
+    private String name;
+    private String email;
+    private String contacNumber;
+}
